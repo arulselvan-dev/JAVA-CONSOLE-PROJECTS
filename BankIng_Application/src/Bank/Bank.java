ce3905c5b847547369baa536fdebb11ccde9e9c8
Arulselvan <126808353+arulselvan-dev@users.noreply.github.com>
2024-06-02 11:27:04 +0530

    Update Bank.java

--- BankIng_Application/src/Bank/Bank.java
+++ BankIng_Application/src/Bank/Bank.java
@@ -8,7 +8,7 @@ public class Bank {
     public static ArrayList<Customer> datas = new ArrayList();
     public static HashMap<Integer,Customer> customerMap = new HashMap<Integer, Customer>();
     public static int customer_id=1;
-    public static long account_no=4000l;
+    public static long account_no=400l;
     public static final double balance=10000;
 
 }
